8743ebe423abf1b5125eab44c0c1aab13706491a
sarsharses <ljotte93@gmail.com>
2025-12-02 19:46:53 +0100

    chore: Angular demo

--- Angular/digipower/src/app/app.ts
+++ Angular/digipower/src/app/app.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+class Student {
+  constructor(
+    public name :string,
+    public age :number,
+    public city :string = '') {
+  }
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected title = 'digipower';
+  author = "Lars";
+  age = 32;
+  city = 'Breda';
+  mainStudent = new Student(this.author, this.age, this.city);
+
+  students = [
+    new Student('Erwin', 12) as Student,
+    new Student('Hans', 18) as Student,
+    new Student('Mike', 24) as Student,
+    ];
+
+  newName = '';
+  newAge = 0;
+
+  birthday() {
+    this.age++;
+  }
+
+  addStudent()
+  {
+    if (this.newName === '' || this.newAge === 0) {
+      return;
+    }
+
+    this.students.push(
+      new Student(this.newName, this.newAge)
+    );
+
+    this.newName = '';
+    this.newAge = 0;
+  }
+
+  clearStudents() {
+    this.students = [];
+  }
 }
